0038a86ec39f3fe2db3e1e235c273604ba11fd7e
antoj15 <99308145+antoj15@users.noreply.github.com>
2022-03-30 11:56:34 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+BOT_TOKEN = '5154238133:AAEyPEd161TSrHnMv276UyN9c6MP7AGMGaw' #token de tu bot
+API_ID = 5085314682 # lo creas en https://my.telegram.org/
+API_HASH = 'fb2335b889dc8277340668f2fa8bee21'  # lo creas en https://my.telegram.org/
+OWNER = [5085314682]
+
+# DATOS DEL MOODLE
+USUARIO = 'user de la nube'
+MOODLE_PASSWORD = 'contraseña'
+USUARIO_ID =  20703 #Tu id de la nube
+ZIP_MB = 100
+MOODLE_URL = 'moodle tu saee'
+
+# CUENTA DE MEGA - este bot sirve par mega tambien
+GMAIL_MEGA = '@gmail.com' 
+PASS_MEGA = 'contraseña'
