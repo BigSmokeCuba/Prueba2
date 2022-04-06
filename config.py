00eaf98aaa51bcc791a959eb05f127f63b19136f
BigSmokeCuba <101540930+BigSmokeCuba@users.noreply.github.com>
2022-04-06 06:57:23 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ API_HASH = '03bca249fdbe6824b1ca6d928a2e60ee'  # lo creas en https://my.telegram
 OWNER = [811906873]
 
 # DATOS DEL MOODLE
-USUARIO = 'darian.borges@estudiantes.fbio.uh.cu'
-MOODLE_PASSWORD = 'darian1995#'
-USUARIO_ID =  4 #Tu id de la nube
-ZIP_MB = 398
-MOODLE_URL = 'https://evea.uh.cu/'
+USUARIO = 'ana-ramos'
+MOODLE_PASSWORD = 'mausi199#'
+USUARIO_ID =  3 #Tu id de la nube
+ZIP_MB = 100
+MOODLE_URL = 'https://eduvirtual.uho.edu.cu/'
 
 # CUENTA DE MEGA - este bot sirve par mega tambien
 GMAIL_MEGA = '@gmail.com' 
